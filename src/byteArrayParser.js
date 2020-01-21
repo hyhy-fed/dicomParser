4e3d2f97dd12fc34feefb614da2e4dff5c7e14d1
Xuegui Yuan <yuanxuegui@huiyihuiying.com>
2020-01-21 09:40:24 +0800

    feat: Add utf-8 string decode

--- src/byteArrayParser.js
+++ src/byteArrayParser.js
@@ -2,9 +2,11 @@
  * Internal helper functions common to parsing byte arrays of any type
  */
 
-const fastGbk = require('fast-gbk');
+import GBK from 'fast-gbk'
+import UTF8 from 'utf-8'
 
-window.fastGbk = fastGbk;
+window.GBK = GBK;
+window.UTF8 = UTF8;
 
 /**
  * Reads a string of 8-bit characters from an array of bytes and advances
@@ -18,7 +20,7 @@ window.fastGbk = fastGbk;
  * @throws error if buffer overread would occur
  * @access private
  */
-export function readFixedString (byteArray, position, length) {
+export function readFixedString (byteArray, position, length, charset) {
   if (length < 0) {
     throw 'dicomParser.readFixedString - length cannot be less than 0';
   }
@@ -37,8 +39,19 @@ export function readFixedString (byteArray, position, length) {
       return result;
     }
     if (byte > 128) {
-      result += fastGbk.decode([byte, byteArray[position + i + 1]]);
-      i++;
+      if (charset === 'ISO_IR 192') { // UTF-8
+        result += String.fromCharCode(UTF8.getCharCode([byte, byteArray[position + i + 1], byteArray[position + i + 2]]))
+        i = i + 2;
+      } else if (charset === 'GBK' || charset === 'GB18030') {
+        // GBK
+        result += GBK.decode([byte, byteArray[position + i + 1]]);
+        i++;
+      } else {
+        // TODO: GBK or UTF-8
+        // GBK
+        result += GBK.decode([byte, byteArray[position + i + 1]]);
+        i++;
+      }
     } else {
       result += String.fromCharCode(byte);
     }
